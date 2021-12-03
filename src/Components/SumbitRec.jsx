0d5a8df5230718c82fee70f264479bb92e935dcb
XuyenHoang <xhoang21@gmail.com>
2021-12-03 15:37:35 -0500

    work please

---
+++ src/Components/SumbitRec.jsx
@@ -0,0 +1,24 @@
+// import React, {useState} from 'react';
+// import '../App.css'
+// import Submitform from './Sumbitform'
+
+// function SumbitRec(props) {
+// const [recipeInput,setRecipeInput]= useState("")    
+// const [submit, setSubmit] = useState("")
+
+//     function handleChange(ev) {
+//         setSubmit(ev.target.value)
+//       }
+//       function handleSubmit(ev) {
+//         ev.preventDefault()
+//         setSubmit(recipeInput)
+//       }  
+//     return (
+//        <Submitform
+//         handleChange={handleChange}
+//         handleSubmit={handleSubmit}
+//        />
+//     );
+// }
+
+// export default SumbitRec;
--- src/Components/Sumbitform.jsx
+++ src/Components/Sumbitform.jsx
@@ -5,9 +5,19 @@ function Sumbitform(props) {
     
     return (
        <img src={UnderConst} alt="UnderConst" className="under"/>
+//        <form onSubmit={handleSubmit} className="searchbox">
+//        <input 
+//        placeholder="search" 
+//        type="text" 
+//        name="search" 
+//        required 
+//        onChange={handleChange}
+//        value={search}
+//        />
+//        <button type="submit">submit</button>
+//        </form>
+   );
+} 
         
-        
-    );
-}
 
 export default Sumbitform;
