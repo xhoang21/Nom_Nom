09d6fe01c184c7f79cce095f5b6bffadc4676805
XuyenHoang <xhoang21@gmail.com>
2021-12-08 18:09:48 -0500

    please work

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React,{useState, useEffect} from 'react';
 import { Route } from "react-router-dom";
 import { Link } from 'react-router-dom';
-import Home from './Components/Home';
+// import Home from './Components/Home';
 import Categories from './Components/Categories';
 import CategoryDetail from './Components/CategoryDetail';
 import Search from './Components/Search';
@@ -11,8 +11,26 @@ import Latest from './Components/Latest';
 import Sumbitform from './Components/Sumbitform';
 import Contact from './Components/Contact';
 import logo from './picture/Nom-Nom-logo/default.png'
+import Coming from "./picture/Coming_Soon/Coming_Soon.001.jpeg"
+import Adds from"./picture/adds/adds.002.jpeg"
 
 function App() {
+  const [meals, SetMeals] = useState([])
+  const Url = `https://www.themealdb.com/api/json/v2/${process.env.REACT_APP_themealdb_KEY}/randomselection.php`
+  useEffect (() => {
+      console.log(Url)
+      fetch (Url)
+      .then(res => {
+          if(res.ok){
+              return res.json()
+          }
+      })
+      .then(json =>{
+          console.log(json)
+          SetMeals(json.meals)
+      })
+      .catch(err => console.log('oooo you did somthing wrong', err))
+  }, [] )
   
   return ( 
     <>
@@ -21,7 +39,7 @@ function App() {
            
             <img src={logo} alt="logo" className="logo"/>
             <div className="nav">
-            <Link to="/home">Home</Link>
+            <Link to="/">Home</Link>
             </div>
             <div className="nav">
             <Link to="/Latest">Newest Recipe</Link>
@@ -42,7 +60,7 @@ function App() {
           </header>      
         </div>
           <main className="main" >
-                    <Route path="/home" component={Home}/>
+                    <Route path="/" component={logo}/>
                     <Route path="/Latest" component={Latest} />
                     <Route path="/Search" component={Search}  />
                     <Route path="/Categories" component={Categories} />
@@ -51,7 +69,48 @@ function App() {
                     <Route path="/SubmitRecipe" component={Sumbitform} />
                     <Route path="/Contact" component={Contact}/>
           </main>
-       
+        <div className="bigContainer">
+          <div className="StaffFavorites">
+          <h2>Staff Favorites! </h2>
+          <div className='HomeContainer'>
+              {meals.map((meals) =>
+                  <Link to= {`/recipe/${meals.idMeal}`} key={meals.idMeal}>
+                      <div className="latestbox">
+                          <div >
+                              <img src={meals.strMealThumb} 
+                                  alt={meals.strMeal} 
+                                  className='latestimg'
+                                  />                                
+                          </div>
+                          <div className='latestname'>
+                              <h3>{meals.strMeal}</h3>                            
+                          </div>
+                      </div>
+                  </Link>   
+                  )}
+                  </div>
+              </div> 
+          <div className="Mid">       
+              <div className="News">
+                      <h2>Food News</h2>
+                      <img src={Coming} alt="Coming" className="Coming"/>
+
+              </div>
+              <div className="seaonal">
+                      <h2>Get your Baking Approns on!</h2>
+                      <img src={Coming} alt="Coming" className="Coming"/>
+              </div>
+          </div>
+          
+          <div className="Adds">
+              <img src={Adds} alt="Adds" className="AddsImg" />
+              <h6>ADVERTISEMENT</h6>
+
+        </div>
+            
+    </div>
+    );
+
           <footer>
           <h6>&copy; 2021 by NomNom inc. All Rights Reserved.</h6>
           <img src={logo} alt="logo" className="footer"/>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,66 +1,66 @@
-import React, { useEffect, useState }from 'react';
-import { Link } from "react-router-dom"
-import Coming from "../picture/Coming_Soon/Coming_Soon.001.jpeg"
-import Adds from"../picture/adds/adds.002.jpeg"
+// import React, { useEffect, useState }from 'react';
+// import { Link } from "react-router-dom"
+// import Coming from "../picture/Coming_Soon/Coming_Soon.001.jpeg"
+// import Adds from"../picture/adds/adds.002.jpeg"
 
-function Home(props) {
-    const [meals, SetMeals] = useState([])
-    const Url = `https://www.themealdb.com/api/json/v2/${process.env.REACT_APP_themealdb_KEY}/randomselection.php`
-    useEffect (() => {
-        console.log(Url)
-        fetch (Url)
-        .then(res => {
-            if(res.ok){
-                return res.json()
-            }
-        })
-        .then(json =>{
-            console.log(json)
-            SetMeals(json.meals)
-        })
-        .catch(err => console.log('oooo you did somthing wrong', err))
-    }, [] )
-    return (
-       <div className="bigContainer">
-        <div className="StaffFavorites">
-        <h2>Staff Favorites! </h2>
-        <div className='HomeContainer'>
-            {meals.map((meals) =>
-                <Link to= {`/recipe/${meals.idMeal}`} key={meals.idMeal}>
-                    <div className="latestbox">
-                        <div >
-                            <img src={meals.strMealThumb} 
-                                alt={meals.strMeal} 
-                                className='latestimg'
-                                />                                
-                        </div>
-                        <div className='latestname'>
-                            <h3>{meals.strMeal}</h3>                            
-                        </div>
-                    </div>
-                </Link>   
-                )}
-                </div>
-            </div> 
-        <div className="Mid">       
-            <div className="News">
-                    <h2>Food News</h2>
-                    <img src={Coming} alt="Coming" className="Coming"/>
+// function Home(props) {
+//     const [meals, SetMeals] = useState([])
+//     const Url = `https://www.themealdb.com/api/json/v2/${process.env.REACT_APP_themealdb_KEY}/randomselection.php`
+//     useEffect (() => {
+//         console.log(Url)
+//         fetch (Url)
+//         .then(res => {
+//             if(res.ok){
+//                 return res.json()
+//             }
+//         })
+//         .then(json =>{
+//             console.log(json)
+//             SetMeals(json.meals)
+//         })
+//         .catch(err => console.log('oooo you did somthing wrong', err))
+//     }, [] )
+//     return (
+//        <div className="bigContainer">
+//         <div className="StaffFavorites">
+//         <h2>Staff Favorites! </h2>
+//         <div className='HomeContainer'>
+//             {meals.map((meals) =>
+//                 <Link to= {`/recipe/${meals.idMeal}`} key={meals.idMeal}>
+//                     <div className="latestbox">
+//                         <div >
+//                             <img src={meals.strMealThumb} 
+//                                 alt={meals.strMeal} 
+//                                 className='latestimg'
+//                                 />                                
+//                         </div>
+//                         <div className='latestname'>
+//                             <h3>{meals.strMeal}</h3>                            
+//                         </div>
+//                     </div>
+//                 </Link>   
+//                 )}
+//                 </div>
+//             </div> 
+//         <div className="Mid">       
+//             <div className="News">
+//                     <h2>Food News</h2>
+//                     <img src={Coming} alt="Coming" className="Coming"/>
 
-            </div>
-            <div className="seaonal">
-                    <h2>Get your Baking Approns on!</h2>
-                    <img src={Coming} alt="Coming" className="Coming"/>
-            </div>
-        </div>
+//             </div>
+//             <div className="seaonal">
+//                     <h2>Get your Baking Approns on!</h2>
+//                     <img src={Coming} alt="Coming" className="Coming"/>
+//             </div>
+//         </div>
         
-        <div className="Adds">
-            <img src={Adds} alt="Adds" className="AddsImg" />
-            <h6>ADVERTISEMENT</h6>
+//         <div className="Adds">
+//             <img src={Adds} alt="Adds" className="AddsImg" />
+//             <h6>ADVERTISEMENT</h6>
 
-        </div>
+//         </div>
             
-    </div>
-    );
-}
-export default Home;           
+//     </div>
+//     );
+// }
+// export default Home;           
